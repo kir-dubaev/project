b770447a32a5a33ab1a7f12336a29f474991c7ef
k-dubaev <kir.dubaev@gmail.com>
2020-12-05 18:58:26 +0300

    First Project

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print("Hello world!")
+
+name = input("Enter your name: ")
+print("Hello,", name)
+
